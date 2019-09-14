3655305fe9aa56ebff3c88d5ce708e21af945c59
vitor.sales <vitor.sales@fengbrasil.com.br>
2019-09-13 23:17:29 -0300

    verificando requisicao
    controle de requisicoes

--- noticias.js
+++ noticias.js
@@ -1,11 +1,24 @@
 //import library http for client's requests 
 let  http = require('http');
-let htmlTemplate = "<html><body><p>Portal de Noticias</p></body></html>"
 
 //create a server
 //argument function(req, res)
 let server = http.createServer(function(req, res){
-    res.end(htmlTemplate);
+
+    let htmlTemplate = req.url;
+
+    switch (htmlTemplate) {
+        case '/tecnologia':
+            res.end("<html<body><p>Tecnologia</body></html>");
+        break;
+        case '/politica':
+            res.end("<html<body><p>Politica</body></html>");
+        break;
+        default:
+            res.end("<html<body><p>Portal de Noticias</body></html>");
+        break;
+    }
+
 });
 
 server.listen(3000);
