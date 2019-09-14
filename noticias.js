b9ca5f0faab340eb55403770f664b01592814665
vitor.sales <vitor.sales@fengbrasil.com.br>
2019-09-13 22:55:38 -0300

    criando servidor e enviando requisicao
    metodo end respondendo com html

--- noticias.js
+++ noticias.js
@@ -1,1 +1,11 @@
-console.log('Criando Site de Noticias');
+//import library http for client's requests 
+let  http = require('http');
+let htmlTemplate = "<html><body><p>Portal de Noticias</p></body></html>"
+
+//create a server
+//argument function(req, res)
+let server = http.createServer(function(req, res){
+    res.end(htmlTemplate);
+});
+
+server.listen(3000);
